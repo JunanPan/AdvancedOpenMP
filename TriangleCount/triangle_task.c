c0e0294fb909f5ad05e6fd3c4ca941cb18d18097
JunanPan <154002155@qq.com>
2024-02-17 00:25:01 -0800

    try1

--- TriangleCount/triangle_task.c
+++ TriangleCount/triangle_task.c
@@ -19,61 +19,66 @@ void input() {
 }
 
 unsigned int count_triangles() {
-    unsigned int delta = 0; 
+  unsigned int delta = 0; 
 
-    #pragma omp parallel
+  #pragma omp parallel
+  {
+    #pragma omp single
     {
-        #pragma omp single
-        for(unsigned int i = 1; i<N-1; i++) {                    
-            #pragma omp task firstprivate(i)
-            {
-                unsigned int *curr_row_x = IA+i;
-                unsigned int *curr_row_A = IA+i+1;
-                unsigned int num_nnz_curr_row_x = *curr_row_A - *curr_row_x;
-                unsigned int *x_col_begin = (JA + *curr_row_x);
-                unsigned int *x_col_end = x_col_begin;
-                unsigned int *row_bound = x_col_begin + num_nnz_curr_row_x;
-                unsigned int col_x_min = 0;
-                unsigned int col_x_max = i-1;
+      // The outer loop traverses over all possible vertices (y). The iteration starts with vertex #1
+      for(unsigned int i = 1; i<N-1; i++) {                    
+        #pragma omp task firstprivate(i) shared(IA, JA) reduction(+:delta)
+        {
+          unsigned int *curr_row_x = IA+i;
+          unsigned int *curr_row_A = IA+i+1;
+          unsigned int num_nnz_curr_row_x = *curr_row_A - *curr_row_x;
+          unsigned int *x_col_begin = (JA + *curr_row_x);
+          unsigned int *x_col_end = x_col_begin;
+          unsigned int *row_bound = x_col_begin + num_nnz_curr_row_x;
+          unsigned int col_x_min = 0;
+          unsigned int col_x_max = i-1;
+          
+          while(x_col_end < row_bound && *x_col_end < col_x_max) ++x_col_end;
+          x_col_end -= (*x_col_end > col_x_max || x_col_end == row_bound);
+              
+          unsigned int *y_col_begin = x_col_end + 1;
+          unsigned int *y_col_end = row_bound-1;
+          unsigned int num_nnz_y = (y_col_end - y_col_begin) + 1;
+          unsigned int num_nnz_x = (x_col_end - x_col_begin) + 1;
+              
+          unsigned int y_col_first = i + 1;
+          unsigned int x_col_first = 0;
+          unsigned int *y_col = y_col_begin;
 
-                while(x_col_end < row_bound && *x_col_end < col_x_max) ++x_col_end;
+          // This is where the real triangle counting begins.
+          // We search through all possible vertices for x
+          for(unsigned int j = 0; j < num_nnz_y; ++j,++y_col) {
+            unsigned int row_index_A = *y_col - y_col_first;             
+            unsigned int *x_col = x_col_begin;           
+            unsigned int num_nnz_A = *(curr_row_A + row_index_A + 1) - *(curr_row_A + row_index_A);
+            unsigned int *A_col = (JA + *(curr_row_A + row_index_A));           
+            unsigned int *A_col_max = A_col + num_nnz_A;
+            
+            // This loop searches through all possible vertices for z.
+            for(unsigned int k = 0; k < num_nnz_x && *A_col <= col_x_max; ++k) {                
+              unsigned int row_index_x = *x_col - x_col_first;               
+              while((*A_col < *x_col) && (A_col < A_col_max)) ++A_col;
 
-                x_col_end -= (*x_col_end > col_x_max || x_col_end == row_bound);
-
-                unsigned int *y_col_begin = x_col_end + 1;
-                unsigned int *y_col_end = row_bound-1;
-                unsigned int num_nnz_y = (y_col_end - y_col_begin) + 1;
-                unsigned int num_nnz_x = (x_col_end - x_col_begin) + 1;
-
-                unsigned int y_col_first = i + 1;
-                unsigned int x_col_first = 0;
-                unsigned int *y_col = y_col_begin;
-
-                for(unsigned int j = 0; j < num_nnz_y; ++j,++y_col) {
-                    unsigned int row_index_A = *y_col - y_col_first;             
-                    unsigned int *x_col = x_col_begin;           
-                    unsigned int num_nnz_A = *(curr_row_A + row_index_A + 1) - *(curr_row_A + row_index_A);
-                    unsigned int *A_col = (JA + *(curr_row_A + row_index_A));           
-                    unsigned int *A_col_max = A_col + num_nnz_A;
-                    
-                    for(unsigned int k = 0; k < num_nnz_x && *A_col <= col_x_max; ++k) {               
-
-                        unsigned int row_index_x = *x_col - x_col_first;               
-                        while((*A_col < *x_col) && (A_col < A_col_max)) ++A_col;
-
-                        delta += (*A_col == row_index_x);
-                        
-                        ++x_col;               
-
-                    }       
-                }
-            }
+              // A triangle is found if the condition returns true
+              delta += (*A_col == row_index_x);
+              
+              ++x_col;               
+            }       
+          }
         }
-        #pragma omp taskwait
+      }
     }
+    #pragma omp taskwait
+  }
   
-    return delta;
+  return delta;
 }
+
 
 int main(int argc, char **argv) {
     int runs = atoi(argv[1]);
